e177440777f3d8c8db12dc4939b3f8dd7153f99b
ArtemShchuchev <shuaa@rambler.ru>
2023-03-16 13:42:02 +0700

    наваял что то страшно глючное

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <execution>
 #include <vector>
+#include <array>
+#include <iomanip>	// std::setw
 #include "SecondaryFunction.h"
 
 /*
@@ -16,58 +18,145 @@
 */
 
 std::once_flag flag;
+// 1000, 10 000, 100 000 и 1 000 000 элементов
+const int SIZEV[]{ 1000, 10000, 100000, 1000000 };
+int numS = sizeof SIZEV / sizeof SIZEV[0];
 
 // сумму 2х векторов складывает в 1ый вектор
-void summVect(std::vector<int>& v1, std::vector<int>& v2, int offset_begin, int num_byte)
+void summVect(std::vector<int>& v1, std::vector<int>& v2, int offset_begin, int num_byte, double& tm)
 {
-	std::call_once(flag, [](){std::wcout << L"Количество аппаратных ядер: "
-		<< std::thread::hardware_concurrency() << "\n\n"; });
+	std::call_once(flag, [](){std::wcout << std::left << L"Количество аппаратных ядер: "
+		<< std::thread::hardware_concurrency() << "\n\n"
+		<< std::setw(11) << " "
+		<< std::setw(11) << "1000"
+		<< std::setw(11) << "10000"
+		<< std::setw(11) << "100000"
+		<< "1000000\n"; });
 
+	auto start = std::chrono::high_resolution_clock::now();
 	for (auto it2 = v2.begin() + offset_begin, it = v1.begin() + offset_begin;
 		it != v1.begin() + offset_begin + num_byte; ++it, ++it2)
 		*it += *it2;
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> time = end - start;
+	tm = time.count();
+}
+// вывод инфо
+void print(std::wstring& str, std::vector<double> workTime)
+{
+	std::wcout << std::left
+		<< std::setw(11) << str
+		<< workTime.at(0) << std::setw(4) << "ms"
+		<< workTime.at(1) << std::setw(4) << "ms"
+		<< workTime.at(2) << std::setw(4) << "ms"
+		<< workTime.at(3) << "ms\n";
 }
 
 int main(int argc, char** argv)
 {
 	printHeader(L"Параллельные вычисления");
 
-	// 1000, 10 000, 100 000 и 1 000 000 элементов
-	int size = 5;
-	std::vector<int> V1(size), V2(size);
-	int num = 0;
-	for (auto& d : V1) d = ++num;
-	for (auto& d : V2) d = 10;
+	const int NUMP(5);
+	std::array<std::wstring, NUMP> potokStr{L"1 поток", L"2 потока", L"4 потока", L"8 потоков", L"16 потоков"};
+	std::vector<double> workTime(numS); // здась буду хранить время работы
+	std::vector<int> V1, V2;	// вектора для сложения
 
-	//auto it_start = V1.begin(), it_end = V1.end();
-
-	auto start = std::chrono::high_resolution_clock::now();
-	std::thread t1(summVect, std::ref(V1), std::ref(V2), 0, size);
-	t1.join();
-	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double, std::milli> time = end - start;
-	std::wcout << L"1 поток = " << time.count() << "\n";
-
-
-
-	start = std::chrono::high_resolution_clock::now();
-	std::thread t2(summVect, std::ref(V1), std::ref(V2), 0, size/2);
-	std::thread t3(summVect, std::ref(V1), std::ref(V2), size/2, size - size / 2);
-	t3.join();
-	t2.join();
-	end = std::chrono::high_resolution_clock::now();
-	time = end - start;
-	std::wcout << L"2а потока = " << time.count() << "\n";
-	
-
-
-
-
-
-	for (auto& d : V1)
+	for (int p = 0; p < NUMP; ++p)
 	{
-		std::wcout << d << " ";
+		for (int i = 0; i < numS; ++i)
+		{
+			V1.resize(SIZEV[i]);
+			V2.resize(SIZEV[i]);
+			for (int j = 0; j < SIZEV[i]; ++j)
+			{
+				V1.at(j) = j + 1;
+				V2.at(j) = 10;
+			}
+
+			if (p == 0)
+			{
+				double time(0.0);
+				std::thread t1(summVect, std::ref(V1), std::ref(V2), 0, SIZEV[i], std::ref(time));
+				t1.join();
+				workTime.at(i) = time;
+			}
+			else if (p == 1)
+			{
+				double time[2]{ 0 };
+				std::thread t2(summVect, std::ref(V1), std::ref(V2), 0, SIZEV[i] / 2, std::ref(time[0]));
+				std::thread t3(summVect, std::ref(V1), std::ref(V2), SIZEV[i] / 2, SIZEV[i] - SIZEV[i] / 2, std::ref(time[1]));
+				t3.join();
+				t2.join();
+				workTime.at(i) = time[0] + time[1];
+			}
+			else if (p == 2)
+			{
+				double time[4]{ 0 };
+				int sizebl = SIZEV[i] / 4;
+				std::thread t4(summVect, std::ref(V1), std::ref(V2), 0, sizebl, std::ref(time[0]));
+				std::thread t5(summVect, std::ref(V1), std::ref(V2), sizebl, sizebl, std::ref(time[1]));
+				std::thread t6(summVect, std::ref(V1), std::ref(V2), sizebl*2, sizebl, std::ref(time[2]));
+				std::thread t7(summVect, std::ref(V1), std::ref(V2), sizebl*3, SIZEV[i] - sizebl * 3, std::ref(time[3]));
+				t4.join();
+				t5.join();
+				t6.join();
+				t7.join();
+				workTime.at(i) = time[0] + time[1] + time[2] + time[3];
+			}
+			else if (p == 3)
+			{
+				double time[8]{ 0 };
+				int sizebl = SIZEV[i] / 8;
+				std::vector<std::thread> t8(8);
+				for (size_t k = 0; k < 8; ++k)
+				{
+					if (k != 7)
+					{
+						t8.at(k) = (std::thread(
+							summVect, std::ref(V1), std::ref(V2), sizebl * k, sizebl, std::ref(time[k]) ));
+					}
+					else
+					{
+						t8.at(k) = (std::thread(
+							summVect, std::ref(V1), std::ref(V2), sizebl * k, SIZEV[i] - sizebl * 7, std::ref(time[k]) ));
+					}
+				}
+
+				for (auto& t : t8) t.join();
+
+				workTime.at(i) = 0;
+				for (size_t k = 0; k < 8; ++k) workTime.at(i) += time[k];
+			}
+			else if (p == 4)
+			{
+				double time[16]{ 0 };
+				int sizebl = SIZEV[i] / 16;
+				std::vector<std::thread> t8(16);
+				for (size_t k = 0; k < 16; ++k)
+				{
+					if (k != 15)
+					{
+						t8.at(k) = (std::thread(
+							summVect, std::ref(V1), std::ref(V2), sizebl * k, sizebl, std::ref(time[k]) ));
+					}
+					else
+					{
+						t8.at(k) = (std::thread(
+							summVect, std::ref(V1), std::ref(V2), sizebl * k, SIZEV[i] - sizebl * 15, std::ref(time[k]) ));
+					}
+				}
+
+				for (auto& t : t8) t.join();
+
+				workTime.at(i) = 0;
+				for (size_t k = 0; k < 16; ++k) workTime.at(i) += time[k];
+			}
+
+		}
+
+		print(potokStr.at(p), workTime);
 	}
+
 
 	
 	std::wcout << "\n";
