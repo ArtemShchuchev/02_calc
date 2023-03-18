2a797e3260305ad12416beab4c06f15f4c436962
ArtemShchuchev <shuaa@rambler.ru>
2023-03-18 12:46:34 +0700

    добавил опр минимума

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ std::once_flag flag;
 // 1000, 10 000, 100 000 и 1 000 000 элементов
 const int SIZEV[]{ 1000, 10000, 100000, 1000000 };
 const int VARSIZE = sizeof SIZEV / sizeof SIZEV[0];	// кол-во вариантов размера массива
+const int NUMP(5);						// кол-во вариантов для потоков
+std::array<std::wstring, NUMP> potokStr{L"1 поток", L"2 потока", L"4 потока", L"8 потоков", L"16 потоков"};
 
 // сумму 2х векторов складывает в 1ый вектор
 void summVect(std::vector<int>& v1, std::vector<int>& v2,
@@ -42,15 +44,41 @@ void summVect(std::vector<int>& v1, std::vector<int>& v2,
 		*it += *it2;
 }
 // вывод инфо
-void print(std::wstring& str, std::vector<double> workTime)
+void print(const std::array <std::array<double, VARSIZE>, NUMP>& workTime)
 {
 	using namespace std;
 	wcout.setf(ios::fixed);
-	wcout << setprecision(3) << left << setw(11) << str
-		<< workTime.at(0) << setw(5) << "ms"
-		<< workTime.at(1) << setw(5) << "ms"
-		<< workTime.at(2) << setw(5) << "ms"
-		<< workTime.at(3) << "ms\n";
+	
+	// заполняю массив минимумов значениями для 1го потока
+	std::array<double, VARSIZE> mintime{ 0 };
+	for (size_t vs = 0; vs < workTime[0].size(); ++vs)
+	{
+		mintime[vs] = workTime[0][vs];
+	}
+
+	// ищу поток с минимальным значением времени, сохраняю время
+	for (size_t vs = 0; vs < workTime[0].size(); ++vs)
+	{
+		for (size_t p = 1; p < workTime.size(); p++)
+		{
+			if (mintime[vs] > workTime[p][vs]) mintime[vs] = workTime[p][vs];
+		}
+	}
+
+	for (size_t p = 0; p < workTime.size(); p++)
+	{
+		wcout << setprecision(3) << left << setw(11) << potokStr.at(p);
+		for (size_t vs = 0; vs < mintime.size(); ++vs)
+		{
+			if (mintime[vs] == workTime[p][vs]) consoleCol(col::br_cyan);
+			wcout << workTime[p][vs] << setw(5) << "ms";
+			consoleCol(col::cancel);
+		}
+		wcout << "\n";
+	}
+	consoleCol(col::br_cyan);
+	std::wcout << L"\nМинимальное время среди потоков.\n";
+	consoleCol(col::cancel);
 }
 
 int pow(int p)
@@ -64,9 +92,8 @@ int main(int argc, char** argv)
 {
 	printHeader(L"Параллельные вычисления");
 
-	const int NUMP(5);						// кол-во вариантов для потоков
-	std::array<std::wstring, NUMP> potokStr{L"1 поток", L"2 потока", L"4 потока", L"8 потоков", L"16 потоков"};
-	std::vector<double> workTime(VARSIZE);	// здась буду хранить время работы, для каждого размера
+	//std::vector<double> workTime(VARSIZE);	// здась буду хранить время работы, для каждого размера
+	std::array <std::array<double, VARSIZE>, NUMP> workTime{ 0 };
 	std::vector<int> V1, V2;				// вектора для сложения
 	
 	std::vector<std::thread> thrs;			// потоки
@@ -76,17 +103,17 @@ int main(int argc, char** argv)
 		int num_potok = pow(p);				// вычисляю кол-во потоков
 		thrs.resize(num_potok);				// устанавливаю кол-во потоков
 
-		for (int i = 0; i < VARSIZE; ++i)
+		for (int vs = 0; vs < VARSIZE; ++vs)
 		{
-			V1.resize(SIZEV[i]);			// устанавливаю размер данных
-			V2.resize(SIZEV[i]);			// устанавливаю размер данных
-			for (int j = 0; j < SIZEV[i]; ++j) // заполняю как то
+			V1.resize(SIZEV[vs]);			// устанавливаю размер данных
+			V2.resize(SIZEV[vs]);			// устанавливаю размер данных
+			for (int j = 0; j < SIZEV[vs]; ++j) // заполняю как то
 			{
 				V1.at(j) = j + 1;
 				V2.at(j) = 10;
 			}
 			////////////////////////////////////
-			int sizebl = SIZEV[i] / num_potok;	// размер блока данных 1го потока
+			int sizebl = SIZEV[vs] / num_potok;	// размер блока данных 1го потока
 
 
 			auto start = std::chrono::steady_clock::now();
@@ -101,7 +128,7 @@ int main(int argc, char** argv)
 				else
 				{
 					thrs.at(k) = (std::thread(
-						summVect, std::ref(V1), std::ref(V2), sizebl * k, SIZEV[i] - sizebl * k, std::ref(start)
+						summVect, std::ref(V1), std::ref(V2), sizebl * k, SIZEV[vs] - sizebl * k, std::ref(start)
 					));
 				}
 				thrs.at(k).join();
@@ -109,12 +136,12 @@ int main(int argc, char** argv)
 			//for (auto& t : thrs) t.join();
 			auto end = std::chrono::steady_clock::now();
 			std::chrono::duration<double, std::milli> delta = end - start;
-			workTime.at(i) = delta.count();
+			workTime.at(p).at(vs) = delta.count();
 			////////////////////////////////////
 		}
-
-		print(potokStr.at(p), workTime);
 	}
+
+		print(workTime);
 
 
 	
